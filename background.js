490816543e564e012fe5387a8855e1b23e788b39
rosamondkim <96304623+rosamondkim@users.noreply.github.com>
2022-07-06 02:03:51 +0900

    Update background.js

--- background.js
+++ background.js
@@ -5,5 +5,5 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
 bgImage.src= `img/${chosenImage}`;
 
-const todaysQuotediv = document.querySelector("#quote");
+
 document.body.appendChild(bgImage);
