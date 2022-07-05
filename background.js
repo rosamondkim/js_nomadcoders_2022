35463ed126e2809bbcf3787172be93e507c1453e
rosamondkim <96304623+rosamondkim@users.noreply.github.com>
2022-07-06 02:02:41 +0900

    Update background.js

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ const images = ["0.jpg","1.jpg","2.jpg","3.jpg"];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
-bgImage.src= `js_nomadcoders_2022/img/${chosenImage}`;
+bgImage.src= `img/${chosenImage}`;
 
 const todaysQuotediv = document.querySelector("#quote");
 document.body.appendChild(bgImage);
